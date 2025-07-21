940d71d6b2728858c87f18b815e24c1073611a95
TaylerWright <44408920+TaylerWright@users.noreply.github.com>
2025-07-20 21:31:47 -0400

    Add files via upload

---
+++ Stock.sql
@@ -0,0 +1,19 @@
+
+CREATE DATABASE test;
+Use test; 
+Create TABLE stock
+(TradeDate char(40), 
+				    opentrade double, 
+                    high double, 
+                    low double, 
+                    closetrade double, 
+                    voloume double, 
+                    Brand_name char(10), 
+                    industry_tag char (15), 
+                    country char (15)
+                    );
+		Select * from stock; 
+        Insert Into stock Values (); 
+        
+  Sele
+        
